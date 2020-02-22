b1306224d3d0c848fcf387f874abb189a3a0a180
adir <adir.elfersy@gmail.com>
2020-02-22 22:40:38 +0200

    init

---
+++ WebAdvert.Web/Controllers/Accounts.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.Extensions.CognitoAuthentication;
+using Amazon.AspNetCore.Identity.Cognito;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebAdvert.Web.Models.Accounts;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAdvert.Web.Controllers
+{
+    public class Accounts : Controller
+    {
+        private readonly SignInManager<CognitoUser> _signInManager;
+        private readonly UserManager<CognitoUser> _userManager;
+        private readonly CognitoUserPool _pool;
+        public Accounts(SignInManager<CognitoUser> signInManager, UserManager<CognitoUser> userManager, CognitoUserPool pool)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _pool = pool;
+        }
+        public async Task<IActionResult> Signup()
+        {
+            var model = new SignupModel();
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Signup(SignupModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _pool.GetUser(model.Email);
+                if(user.Status != null)
+                {
+                    ModelState.AddModelError("UserExists", "User with this email already exists");
+                    return View(model);
+                }
+                user.Attributes.Add(CognitoAttribute.Name.AttributeName, model.Email);
+                var createdUser = await _userManager.CreateAsync(user, model.Password).ConfigureAwait(false);
+                if(createdUser.Succeeded)
+                {
+                    RedirectToAction("Confirm");
+                }
+            }
+            return View();
+        }
+    }
+}
